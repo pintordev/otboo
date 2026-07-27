a1e26797011a5d4ce6f5643d668e001506b6b2e2
pintor.dev <pintor.dev@gmail.com>
2026-07-27 21:40:45 +0900

    test(red): KakaoLocalClient 실제 API 호출 검증 테스트 작성

---
+++ src/test/java/com/sprint/mission/otboo/external/kakao/KakaoLocalClientTest.java
@@ -0,0 +1,42 @@
+package com.sprint.mission.otboo.external.kakao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.sprint.mission.otboo.external.kakao.dto.KakaoRegionResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoLocalClientTest {
+
+  @Autowired
+  private KakaoLocalClient kakaoLocalClient;
+
+  @Value("${weather.kakao.rest-api-key}")
+  private String restApiKey;
+
+  @Nested
+  @DisplayName("GetRegionCode")
+  class GetRegionCode {
+
+    @Test
+    @DisplayName("실제_카카오_API를_호출하면_행정동_정보를_받는다")
+    void 실제_카카오_API를_호출하면_행정동_정보를_받는다() {
+      // given
+      String authorization = "KakaoAK " + restApiKey;
+
+      // when
+      KakaoRegionResponse response = kakaoLocalClient.getRegionCode(authorization, 126.9884121,
+          37.5674783);
+
+      // then
+      assertThat(response.documents()).isNotEmpty();
+      assertThat(response.documents())
+          .anySatisfy(doc -> assertThat(doc.regionType()).isEqualTo("H"));
+    }
+  }
+}
