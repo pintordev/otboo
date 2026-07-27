4c14412aded6a7f33564e6a3d79e262aae341e48
pintor.dev <pintor.dev@gmail.com>
2026-07-27 22:29:26 +0900

    test(green): WeatherApi 및 WeatherController GET /api/weathers 구현

---
+++ src/main/java/com/sprint/mission/otboo/domain/weathernotification/weather/controller/WeatherController.java
@@ -0,0 +1,29 @@
+package com.sprint.mission.otboo.domain.weathernotification.weather.controller;
+
+import com.sprint.mission.otboo.domain.weathernotification.weather.controller.api.WeatherApi;
+import com.sprint.mission.otboo.domain.weathernotification.weather.dto.WeatherAPILocation;
+import com.sprint.mission.otboo.domain.weathernotification.weather.dto.WeatherDto;
+import com.sprint.mission.otboo.domain.weathernotification.weather.service.WeatherService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/weathers")
+public class WeatherController implements WeatherApi {
+
+  private final WeatherService weatherService;
+
+  @Override
+  @GetMapping
+  public ResponseEntity<List<WeatherDto>> getWeather(
+      @RequestParam("longitude") double longitude,
+      @RequestParam("latitude") double latitude) {
+    return ResponseEntity.ok(weatherService.getWeather(latitude, longitude));
+  }
+}
---
+++ src/main/java/com/sprint/mission/otboo/domain/weathernotification/weather/controller/api/WeatherApi.java
@@ -0,0 +1,20 @@
+package com.sprint.mission.otboo.domain.weathernotification.weather.controller.api;
+
+import com.sprint.mission.otboo.domain.weathernotification.weather.dto.WeatherDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "날씨 관리", description = "날씨 관련 API")
+public interface WeatherApi {
+
+  @Operation(summary = "날씨 정보 조회", description = "날씨 정보 조회 API")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "날씨 조회 성공"),
+      @ApiResponse(responseCode = "400", description = "날씨 조회 실패")
+  })
+  ResponseEntity<List<WeatherDto>> getWeather(double longitude, double latitude);
+}
