a78e6c4b1de0eb43a09409403946ea06d7770675
pintor.dev <pintor.dev@gmail.com>
2026-07-27 21:43:41 +0900

    test(green): KakaoRegionParser 행정동 위치명 추출 구현

---
+++ src/main/java/com/sprint/mission/otboo/external/kakao/KakaoRegionParser.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.otboo.external.kakao;
+
+import com.sprint.mission.otboo.external.kakao.dto.KakaoRegionResponse;
+import com.sprint.mission.otboo.external.kakao.dto.KakaoRegionResponse.Document;
+import java.util.List;
+
+public class KakaoRegionParser {
+
+  private static final String ADMINISTRATIVE_DONG_TYPE = "H";
+
+  public List<String> toLocationNames(KakaoRegionResponse response) {
+    return response.documents().stream()
+        .filter(document -> ADMINISTRATIVE_DONG_TYPE.equals(document.regionType()))
+        .findFirst()
+        .map(document -> List.of(
+            document.region1depthName(),
+            document.region2depthName(),
+            document.region3depthName(),
+            document.region4depthName()
+        ))
+        .orElseThrow();
+  }
+}
