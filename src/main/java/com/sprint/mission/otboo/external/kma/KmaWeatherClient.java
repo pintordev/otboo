4fffa171c454f70beafe0c990c82cd34e4f55691
pintor.dev <pintor.dev@gmail.com>
2026-07-27 21:04:16 +0900

    test(green): KmaWeatherClient Feign 인터페이스 구현

---
+++ src/main/java/com/sprint/mission/otboo/external/kma/KmaWeatherClient.java
@@ -0,0 +1,22 @@
+package com.sprint.mission.otboo.external.kma;
+
+import com.sprint.mission.otboo.external.kma.dto.KmaWeatherResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "kmaWeatherClient", url = "${weather.kma.base-url}")
+public interface KmaWeatherClient {
+
+  @GetMapping
+  KmaWeatherResponse getVillageForecast(
+      @RequestParam("serviceKey") String serviceKey,
+      @RequestParam("numOfRows") int numOfRows,
+      @RequestParam("pageNo") int pageNo,
+      @RequestParam("dataType") String dataType,
+      @RequestParam("base_date") String baseDate,
+      @RequestParam("base_time") String baseTime,
+      @RequestParam("nx") int nx,
+      @RequestParam("ny") int ny
+  );
+}
---
+++ src/main/java/com/sprint/mission/otboo/external/kma/dto/KmaWeatherResponse.java
@@ -0,0 +1,27 @@
+package com.sprint.mission.otboo.external.kma.dto;
+
+import java.util.List;
+
+public record KmaWeatherResponse(Response response) {
+
+  public record Response(Header header, Body body) {
+
+  }
+
+  public record Header(String resultCode, String resultMsg) {
+
+  }
+
+  public record Body(String dataType, Items items, int pageNo, int numOfRows, int totalCount) {
+
+  }
+
+  public record Items(List<Item> item) {
+
+  }
+
+  public record Item(String baseDate, String baseTime, String category, String fcstDate,
+      String fcstTime, String fcstValue, int nx, int ny) {
+
+  }
+}
