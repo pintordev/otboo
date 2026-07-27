f92c4815159440f4b237fa0c6d6927d26d5bbc4e
pintor.dev <pintor.dev@gmail.com>
2026-07-27 12:14:13 +0900

    test(green): NoResourceFoundException 처리 구현

--- src/main/java/com/sprint/mission/otboo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sprint/mission/otboo/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,22 @@
 package com.sprint.mission.otboo.global.exception;
 
+import com.sprint.mission.otboo.global.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoResourceFound(NoResourceFoundException e) {
+        log.warn("[{}] {}", e.getClass().getSimpleName(), e.getMessage());
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(new ErrorResponse(e.getClass().getSimpleName(), "요청한 리소스를 찾을 수 없습니다.", null));
+    }
 }
