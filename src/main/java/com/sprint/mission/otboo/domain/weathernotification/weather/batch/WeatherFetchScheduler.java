61a3157732061e0e62be6917f0a534ea9b27dbbd
pintor.dev <pintor.dev@gmail.com>
2026-07-29 13:20:08 +0900

    test(green): WeatherFetchScheduler 구현

---
+++ src/main/java/com/sprint/mission/otboo/domain/weathernotification/weather/batch/WeatherFetchScheduler.java
@@ -0,0 +1,21 @@
+package com.sprint.mission.otboo.domain.weathernotification.weather.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WeatherFetchScheduler {
+
+  private final WeatherFetchService weatherFetchService;
+
+  @Scheduled(cron = "0 30 2,5,8,11,14,17,20,23 * * *", zone = "Asia/Seoul")
+  public void fetch() {
+    log.info("날씨 수집 배치 시작");
+    weatherFetchService.execute();
+    log.info("날씨 수집 배치 완료");
+  }
+}
