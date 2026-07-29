d877a9ab4d9af0da6f67bba7fe40652bb911bf70
pintor.dev <pintor.dev@gmail.com>
2026-07-29 13:19:57 +0900

    test(red): WeatherFetchScheduler 테스트 추가

---
+++ src/test/java/com/sprint/mission/otboo/domain/weathernotification/weather/batch/WeatherFetchSchedulerTest.java
@@ -0,0 +1,38 @@
+package com.sprint.mission.otboo.domain.weathernotification.weather.batch;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class WeatherFetchSchedulerTest {
+
+  @InjectMocks
+  private WeatherFetchScheduler scheduler;
+
+  @Mock
+  private WeatherFetchService weatherFetchService;
+
+  @Nested
+  @DisplayName("Fetch")
+  class Fetch {
+
+    @Test
+    @DisplayName("WeatherFetchService_execute만_호출하고_다른_로직은_없다")
+    void WeatherFetchService_execute만_호출하고_다른_로직은_없다() {
+      // when
+      scheduler.fetch();
+
+      // then
+      verify(weatherFetchService).execute();
+      verifyNoMoreInteractions(weatherFetchService);
+    }
+  }
+}
