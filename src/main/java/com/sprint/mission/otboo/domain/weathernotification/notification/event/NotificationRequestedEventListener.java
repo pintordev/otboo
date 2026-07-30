71c7b1bfc02b6af96d3c756f7d187b085d18c908
pintor.dev <pintor.dev@gmail.com>
2026-07-30 10:41:01 +0900

    test(green): NotificationRequestedEventListener 구현

---
+++ src/main/java/com/sprint/mission/otboo/domain/weathernotification/notification/event/NotificationRequestedEventListener.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.otboo.domain.weathernotification.notification.event;
+
+import com.sprint.mission.otboo.domain.weathernotification.notification.dto.NotificationDto;
+import com.sprint.mission.otboo.domain.weathernotification.notification.service.NotificationService;
+import com.sprint.mission.otboo.global.event.NotificationRequestedEvent;
+import com.sprint.mission.otboo.global.sse.SseService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@RequiredArgsConstructor
+@Component
+public class NotificationRequestedEventListener {
+
+  private final NotificationService notificationService;
+  private final SseService sseService;
+
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+  public void on(NotificationRequestedEvent event) {
+    List<NotificationDto> notificationDtos = notificationService.create(event);
+    sseService.send(notificationDtos, "notifications");
+  }
+}
