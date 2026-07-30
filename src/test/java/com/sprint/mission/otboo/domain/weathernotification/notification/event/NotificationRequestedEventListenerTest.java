98be274c9c9cde642a864830507124655e489099
pintor.dev <pintor.dev@gmail.com>
2026-07-30 10:40:23 +0900

    test(red): NotificationRequestedEventListener 테스트 추가

---
+++ src/test/java/com/sprint/mission/otboo/domain/weathernotification/notification/event/NotificationRequestedEventListenerTest.java
@@ -0,0 +1,55 @@
+package com.sprint.mission.otboo.domain.weathernotification.notification.event;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.sprint.mission.otboo.domain.weathernotification.notification.dto.NotificationDto;
+import com.sprint.mission.otboo.domain.weathernotification.notification.service.NotificationService;
+import com.sprint.mission.otboo.global.event.NotificationLevel;
+import com.sprint.mission.otboo.global.event.NotificationRequestedEvent;
+import com.sprint.mission.otboo.global.sse.SseService;
+import java.time.Instant;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationRequestedEventListenerTest {
+
+    @InjectMocks
+    private NotificationRequestedEventListener notificationRequestedEventListener;
+
+    @Mock
+    private NotificationService notificationService;
+    @Mock
+    private SseService sseService;
+
+    @Nested
+    @DisplayName("on")
+    class On {
+
+        @Test
+        @DisplayName("이벤트를_받으면_알림을_생성하고_그_결과를_notifications_이벤트명으로_SSE_발행한다")
+        void 이벤트를_받으면_알림을_생성하고_그_결과를_notifications_이벤트명으로_SSE_발행한다() {
+            // given
+            NotificationRequestedEvent event = new NotificationRequestedEvent(
+                    Set.of(UUID.randomUUID()), "제목", "내용", NotificationLevel.INFO);
+            List<NotificationDto> notificationDtos = List.of(new NotificationDto(
+                    UUID.randomUUID(), Instant.now(), UUID.randomUUID(), "제목", "내용", NotificationLevel.INFO));
+            given(notificationService.create(event)).willReturn(notificationDtos);
+
+            // when
+            notificationRequestedEventListener.on(event);
+
+            // then
+            verify(sseService).send(notificationDtos, "notifications");
+        }
+    }
+}
