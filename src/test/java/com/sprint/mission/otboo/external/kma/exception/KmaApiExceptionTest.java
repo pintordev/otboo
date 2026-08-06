f7ccc8c84da9cedd7c595eb46d86f60064d14221
pintor.dev <pintor.dev@gmail.com>
2026-08-06 17:58:14 +0900

    test(red): KmaApiException.wrap(cause) 팩토리 테스트 추가

--- src/test/java/com/sprint/mission/otboo/external/kma/exception/KmaApiExceptionTest.java
+++ src/test/java/com/sprint/mission/otboo/external/kma/exception/KmaApiExceptionTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 class KmaApiExceptionTest {
 
@@ -23,6 +24,41 @@ class KmaApiExceptionTest {
       assertThat(exception.getDetails())
           .containsEntry("resultCode", "UNKNOWN")
           .containsEntry("resultMsg", "UNKNOWN");
+    }
+  }
+
+  @Nested
+  @DisplayName("Wrap")
+  class Wrap {
+
+    @Test
+    @DisplayName("원인_예외를_감싸_BAD_GATEWAY_상태의_KmaApiException을_생성한다")
+    void 원인_예외를_감싸_BAD_GATEWAY_상태의_KmaApiException을_생성한다() {
+      // given
+      IllegalStateException cause = new IllegalStateException("커넥션 리셋");
+
+      // when
+      KmaApiException exception = KmaApiException.wrap(cause);
+
+      // then
+      assertThat(exception.getStatus()).isEqualTo(HttpStatus.BAD_GATEWAY);
+      assertThat(exception.getDetails())
+          .containsEntry("causeType", "IllegalStateException")
+          .containsEntry("causeMessage", "커넥션 리셋");
+      assertThat(exception.getCause()).isSameAs(cause);
+    }
+
+    @Test
+    @DisplayName("원인_예외의_메시지가_null이면_UNKNOWN으로_기록된다")
+    void 원인_예외의_메시지가_null이면_UNKNOWN으로_기록된다() {
+      // given
+      IllegalStateException cause = new IllegalStateException();
+
+      // when
+      KmaApiException exception = KmaApiException.wrap(cause);
+
+      // then
+      assertThat(exception.getDetails()).containsEntry("causeMessage", "UNKNOWN");
     }
   }
 }
