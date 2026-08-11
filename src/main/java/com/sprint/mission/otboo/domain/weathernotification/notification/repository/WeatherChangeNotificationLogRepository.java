fdce184105474f6739928e2dce271728c9558973
pintor.dev <pintor.dev@gmail.com>
2026-08-11 18:47:16 +0900

    test(green): findByWeatherGridAndForecastAt 조회 쿼리 구현

--- src/main/java/com/sprint/mission/otboo/domain/weathernotification/notification/repository/WeatherChangeNotificationLogRepository.java
+++ src/main/java/com/sprint/mission/otboo/domain/weathernotification/notification/repository/WeatherChangeNotificationLogRepository.java
@@ -1,10 +1,15 @@
 package com.sprint.mission.otboo.domain.weathernotification.notification.repository;
 
 import com.sprint.mission.otboo.domain.weathernotification.notification.entity.WeatherChangeNotificationLog;
+import com.sprint.mission.otboo.domain.weathernotification.weather.entity.WeatherGrid;
+import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeatherChangeNotificationLogRepository
     extends JpaRepository<WeatherChangeNotificationLog, UUID> {
 
+  Optional<WeatherChangeNotificationLog> findByWeatherGridAndForecastAt(
+      WeatherGrid weatherGrid, Instant forecastAt);
 }
