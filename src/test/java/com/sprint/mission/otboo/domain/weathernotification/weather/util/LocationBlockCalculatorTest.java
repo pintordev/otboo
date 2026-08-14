aa319ab6f35a6d696fd4727ce8330b27be1dca1e
pintor.dev <pintor.dev@gmail.com>
2026-08-14 12:10:20 +0900

    test(red): LocationBlockCalculator 블록 크기를 500m 기준으로 재검증하는 테스트로 갱신

--- src/test/java/com/sprint/mission/otboo/domain/weathernotification/weather/util/LocationBlockCalculatorTest.java
+++ src/test/java/com/sprint/mission/otboo/domain/weathernotification/weather/util/LocationBlockCalculatorTest.java
@@ -14,12 +14,12 @@ class LocationBlockCalculatorTest {
   class ToBlock {
 
     @Test
-    @DisplayName("50m_이내로_가까운_좌표는_같은_블록으로_계산된다")
-    void 오십미터_이내로_가까운_좌표는_같은_블록으로_계산된다() {
+    @DisplayName("500m_이내로_가까운_좌표는_같은_블록으로_계산된다")
+    void 오백미터_이내로_가까운_좌표는_같은_블록으로_계산된다() {
       // given
       double latitude = 37.5670000;
       double longitude = 126.9880000;
-      double nearbyLatitude = 37.5670898; // 약 10m 북쪽
+      double nearbyLatitude = 37.5678983; // 약 100m 북쪽
 
       // when
       BlockIndex block = LocationBlockCalculator.toBlock(latitude, longitude);
@@ -30,12 +30,12 @@ class LocationBlockCalculatorTest {
     }
 
     @Test
-    @DisplayName("위도가_200m_이상_떨어지면_다른_블록으로_계산된다")
-    void 위도가_이백미터_이상_떨어지면_다른_블록으로_계산된다() {
+    @DisplayName("위도가_2km_이상_떨어지면_다른_블록으로_계산된다")
+    void 위도가_이천미터_이상_떨어지면_다른_블록으로_계산된다() {
       // given
       double latitude = 37.5670000;
       double longitude = 126.9880000;
-      double farLatitude = 37.5687966; // 약 200m 북쪽
+      double farLatitude = 37.5849662; // 약 2000m 북쪽
 
       // when
       BlockIndex block = LocationBlockCalculator.toBlock(latitude, longitude);
@@ -47,12 +47,12 @@ class LocationBlockCalculatorTest {
     }
 
     @Test
-    @DisplayName("경도가_200m_이상_떨어지면_다른_블록으로_계산된다")
-    void 경도가_이백미터_이상_떨어지면_다른_블록으로_계산된다() {
+    @DisplayName("경도가_2km_이상_떨어지면_다른_블록으로_계산된다")
+    void 경도가_이천미터_이상_떨어지면_다른_블록으로_계산된다() {
       // given
       double latitude = 37.5670000;
       double longitude = 126.9880000;
-      double farLongitude = 126.9902350; // 약 200m 동쪽
+      double farLongitude = 127.0103500; // 약 2000m 동쪽
 
       // when
       BlockIndex block = LocationBlockCalculator.toBlock(latitude, longitude);
