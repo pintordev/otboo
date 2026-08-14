7379bd719777ffdb5c1be9cc6b39e887bf32c930
pintor.dev <pintor.dev@gmail.com>
2026-08-14 18:33:31 +0900

    test(red): KmaForecastFetcher.fetchSlots가 슬롯순 정렬된 슬롯 예보를 반환하는 테스트 추가

--- src/test/java/com/sprint/mission/otboo/external/kma/KmaForecastFetcherTest.java
+++ src/test/java/com/sprint/mission/otboo/external/kma/KmaForecastFetcherTest.java
@@ -16,6 +16,7 @@ import com.sprint.mission.otboo.external.kma.dto.DailyWeatherForecastDto;
 import com.sprint.mission.otboo.external.kma.dto.KmaWeatherResponse;
 import com.sprint.mission.otboo.external.kma.dto.KmaWeatherResponse.Header;
 import com.sprint.mission.otboo.external.kma.dto.KmaWeatherResponse.Response;
+import com.sprint.mission.otboo.external.kma.dto.WeatherForecastSlotDto;
 import com.sprint.mission.otboo.external.kma.exception.KmaApiException;
 import feign.FeignException;
 import feign.Request;
@@ -191,6 +192,48 @@ class KmaForecastFetcherTest {
           .reason("Service Unavailable")
           .headers(Map.of())
           .build();
+    }
+  }
+
+  @Nested
+  @DisplayName("FetchSlots")
+  class FetchSlots {
+
+    @Test
+    @DisplayName("기상청_호출_후_날짜_슬롯순으로_정렬된_슬롯_예보를_반환한다")
+    void 기상청_호출_후_날짜_슬롯순으로_정렬된_슬롯_예보를_반환한다() {
+      // given
+      KmaForecastFetcher fetcher = new KmaForecastFetcher(kmaWeatherClient, kmaForecastParser,
+          "kma-service-key");
+      KmaGridPoint grid = FIXTURE_MONKEY.giveMeBuilder(KmaGridPoint.class)
+          .set("nx", 60)
+          .set("ny", 127)
+          .sample();
+      BaseTime baseTime = FIXTURE_MONKEY.giveMeBuilder(BaseTime.class)
+          .set("baseDate", "20260727")
+          .set("baseTime", "1700")
+          .sample();
+
+      KmaWeatherResponse response = new KmaWeatherResponse(
+          new Response(new Header("00", "정상"), null));
+      given(kmaWeatherClient.getVillageForecast("kma-service-key", 2000, 1, "JSON", "20260727",
+          "1700", 60, 127)).willReturn(response);
+
+      WeatherForecastSlotDto late = FIXTURE_MONKEY.giveMeBuilder(WeatherForecastSlotDto.class)
+          .set("date", LocalDate.of(2026, 7, 27))
+          .set("slotAt", Instant.parse("2026-07-27T09:00:00Z"))
+          .sample();
+      WeatherForecastSlotDto early = FIXTURE_MONKEY.giveMeBuilder(WeatherForecastSlotDto.class)
+          .set("date", LocalDate.of(2026, 7, 27))
+          .set("slotAt", Instant.parse("2026-07-27T00:00:00Z"))
+          .sample();
+      given(kmaForecastParser.parseSlotForecast(response)).willReturn(List.of(late, early));
+
+      // when
+      List<WeatherForecastSlotDto> result = fetcher.fetchSlots(grid, baseTime);
+
+      // then
+      assertThat(result).containsExactly(early, late);
     }
   }
 }
