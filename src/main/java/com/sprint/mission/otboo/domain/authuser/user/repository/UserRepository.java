2fa491795a0c8fd265c7fb61d0d7144b68158c5f
pintor.dev <pintor.dev@gmail.com>
2026-08-18 21:46:41 +0900

    test(green): UserRepository.findAllIds() 구현

--- src/main/java/com/sprint/mission/otboo/domain/authuser/user/repository/UserRepository.java
+++ src/main/java/com/sprint/mission/otboo/domain/authuser/user/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.sprint.mission.otboo.domain.authuser.user.repository;
 import com.sprint.mission.otboo.domain.authuser.user.entity.User;
 import com.sprint.mission.otboo.domain.authuser.user.repository.querydsl.UserCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, UUID>, UserCustomRep
   boolean existsByEmail(String email);
 
   Optional<User> findByEmail(String email);
+
+  @Query("SELECT u.id FROM User u")
+  List<UUID> findAllIds();
 }
