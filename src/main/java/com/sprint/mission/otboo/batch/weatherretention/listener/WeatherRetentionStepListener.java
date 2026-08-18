3a6c61c6f4f371d1e8f9988b1eb39350c4f48708
pintor.dev <pintor.dev@gmail.com>
2026-08-18 20:52:56 +0900

    refactor: Google Java Style Guide 준수

--- src/main/java/com/sprint/mission/otboo/batch/weatherretention/listener/WeatherRetentionStepListener.java
+++ src/main/java/com/sprint/mission/otboo/batch/weatherretention/listener/WeatherRetentionStepListener.java
@@ -27,10 +27,12 @@ public class WeatherRetentionStepListener implements StepExecutionListener {
     Duration duration = Duration.between(stepExecution.getStartTime(), stepExecution.getEndTime());
     ExitStatus exitStatus = stepExecution.getExitStatus();
 
-    weatherRetentionMetrics.countCleaned(stepExecution.getStepName(), stepExecution.getWriteCount());
+    weatherRetentionMetrics.countCleaned(stepExecution.getStepName(),
+        stepExecution.getWriteCount());
 
     if (ExitStatus.FAILED.getExitCode().equals(exitStatus.getExitCode())) {
-      log.error("WeatherRetention Step 실패 | readCount={}, writeCount={}, duration={}, exitStatus={}",
+      log.error(
+          "WeatherRetention Step 실패 | readCount={}, writeCount={}, duration={}, exitStatus={}",
           stepExecution.getReadCount(), stepExecution.getWriteCount(), duration, exitStatus);
     } else {
       log.info("WeatherRetention Step 완료 | readCount={}, writeCount={}, duration={}",
