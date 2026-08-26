ee2b5787393b6db70929e318de595d65b3639da1
pintor.dev <pintor.dev@gmail.com>
2026-08-26 22:08:17 +0900

    test: SingleFlightLeaseTimeoutValidatorTest @DisplayName 한글화 및 fixture를 FixtureMonkey로 변경

--- src/test/java/com/sprint/mission/otboo/domain/weathernotification/weather/config/SingleFlightLeaseTimeoutValidatorTest.java
+++ src/test/java/com/sprint/mission/otboo/domain/weathernotification/weather/config/SingleFlightLeaseTimeoutValidatorTest.java
@@ -3,6 +3,8 @@ package com.sprint.mission.otboo.domain.weathernotification.weather.config;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
 import com.sprint.mission.otboo.external.kakao.KakaoFeignProperties;
 import com.sprint.mission.otboo.external.kma.KmaFeignProperties;
 import java.time.Duration;
@@ -12,20 +14,32 @@ import org.junit.jupiter.api.Test;
 
 class SingleFlightLeaseTimeoutValidatorTest {
 
+  private static final FixtureMonkey FIXTURE_MONKEY = FixtureMonkey.builder()
+      .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+      .build();
+
   @Nested
-  @DisplayName("Validate")
+  @DisplayName("검증")
   class Validate {
 
     @Test
     @DisplayName("kma_kakao_타임아웃_합이_lock_ttl보다_짧으면_예외를_던지지_않는다")
     void kma_kakao_타임아웃_합이_lock_ttl보다_짧으면_예외를_던지지_않는다() {
       // given
-      SingleFlightProperties singleFlightProperties = new SingleFlightProperties(
-          Duration.ofSeconds(10));
-      KmaFeignProperties kmaFeignProperties = new KmaFeignProperties(
-          Duration.ofSeconds(2), Duration.ofSeconds(5));
-      KakaoFeignProperties kakaoFeignProperties = new KakaoFeignProperties(
-          Duration.ofSeconds(2), Duration.ofSeconds(5));
+      SingleFlightProperties singleFlightProperties = FIXTURE_MONKEY
+          .giveMeBuilder(SingleFlightProperties.class)
+          .set("lockTtl", Duration.ofSeconds(10))
+          .sample();
+      KmaFeignProperties kmaFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KmaFeignProperties.class)
+          .set("connect", Duration.ofSeconds(2))
+          .set("read", Duration.ofSeconds(5))
+          .sample();
+      KakaoFeignProperties kakaoFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KakaoFeignProperties.class)
+          .set("connect", Duration.ofSeconds(2))
+          .set("read", Duration.ofSeconds(5))
+          .sample();
       SingleFlightLeaseTimeoutValidator validator = new SingleFlightLeaseTimeoutValidator(
           singleFlightProperties, kmaFeignProperties, kakaoFeignProperties);
 
@@ -37,12 +51,20 @@ class SingleFlightLeaseTimeoutValidatorTest {
     @DisplayName("kma_타임아웃_합이_lock_ttl_이상이면_예외를_던진다")
     void kma_타임아웃_합이_lock_ttl_이상이면_예외를_던진다() {
       // given
-      SingleFlightProperties singleFlightProperties = new SingleFlightProperties(
-          Duration.ofSeconds(10));
-      KmaFeignProperties kmaFeignProperties = new KmaFeignProperties(
-          Duration.ofSeconds(5), Duration.ofSeconds(5));
-      KakaoFeignProperties kakaoFeignProperties = new KakaoFeignProperties(
-          Duration.ofSeconds(2), Duration.ofSeconds(5));
+      SingleFlightProperties singleFlightProperties = FIXTURE_MONKEY
+          .giveMeBuilder(SingleFlightProperties.class)
+          .set("lockTtl", Duration.ofSeconds(10))
+          .sample();
+      KmaFeignProperties kmaFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KmaFeignProperties.class)
+          .set("connect", Duration.ofSeconds(5))
+          .set("read", Duration.ofSeconds(5))
+          .sample();
+      KakaoFeignProperties kakaoFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KakaoFeignProperties.class)
+          .set("connect", Duration.ofSeconds(2))
+          .set("read", Duration.ofSeconds(5))
+          .sample();
       SingleFlightLeaseTimeoutValidator validator = new SingleFlightLeaseTimeoutValidator(
           singleFlightProperties, kmaFeignProperties, kakaoFeignProperties);
 
@@ -54,12 +76,20 @@ class SingleFlightLeaseTimeoutValidatorTest {
     @DisplayName("kakao_타임아웃_합이_lock_ttl_이상이면_예외를_던진다")
     void kakao_타임아웃_합이_lock_ttl_이상이면_예외를_던진다() {
       // given
-      SingleFlightProperties singleFlightProperties = new SingleFlightProperties(
-          Duration.ofSeconds(10));
-      KmaFeignProperties kmaFeignProperties = new KmaFeignProperties(
-          Duration.ofSeconds(2), Duration.ofSeconds(5));
-      KakaoFeignProperties kakaoFeignProperties = new KakaoFeignProperties(
-          Duration.ofSeconds(8), Duration.ofSeconds(2));
+      SingleFlightProperties singleFlightProperties = FIXTURE_MONKEY
+          .giveMeBuilder(SingleFlightProperties.class)
+          .set("lockTtl", Duration.ofSeconds(10))
+          .sample();
+      KmaFeignProperties kmaFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KmaFeignProperties.class)
+          .set("connect", Duration.ofSeconds(2))
+          .set("read", Duration.ofSeconds(5))
+          .sample();
+      KakaoFeignProperties kakaoFeignProperties = FIXTURE_MONKEY
+          .giveMeBuilder(KakaoFeignProperties.class)
+          .set("connect", Duration.ofSeconds(8))
+          .set("read", Duration.ofSeconds(2))
+          .sample();
       SingleFlightLeaseTimeoutValidator validator = new SingleFlightLeaseTimeoutValidator(
           singleFlightProperties, kmaFeignProperties, kakaoFeignProperties);
 
