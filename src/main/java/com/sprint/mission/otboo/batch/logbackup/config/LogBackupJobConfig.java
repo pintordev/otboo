a3713bb6dcca51d6506610be53ab9be1a276019a
pintor.dev <pintor.dev@gmail.com>
2026-08-28 13:28:38 +0900

    test(green): LogBackupJobConfig 구현

---
+++ src/main/java/com/sprint/mission/otboo/batch/logbackup/config/LogBackupJobConfig.java
@@ -0,0 +1,66 @@
+package com.sprint.mission.otboo.batch.logbackup.config;
+
+import com.sprint.mission.otboo.batch.logbackup.dto.LogContent;
+import com.sprint.mission.otboo.batch.logbackup.dto.UploadPayload;
+import com.sprint.mission.otboo.batch.logbackup.exception.LogBackupFailedException;
+import com.sprint.mission.otboo.batch.logbackup.listener.LogBackupJobListener;
+import com.sprint.mission.otboo.batch.logbackup.listener.LogBackupStepListener;
+import com.sprint.mission.otboo.batch.logbackup.processor.LogBackupProcessor;
+import com.sprint.mission.otboo.batch.logbackup.reader.LogBackupReader;
+import com.sprint.mission.otboo.batch.logbackup.writer.LogBackupWriter;
+import com.sprint.mission.otboo.global.batch.SkipLoggingListener;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.job.Job;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.Step;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.TransientDataAccessException;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(LogBackupProperties.class)
+public class LogBackupJobConfig {
+
+  private final JobRepository jobRepository;
+  private final PlatformTransactionManager transactionManager;
+
+  private final LogBackupJobListener logBackupJobListener;
+  private final LogBackupStepListener logBackupStepListener;
+  private final SkipLoggingListener skipLoggingListener;
+  private final LogBackupReader logBackupReader;
+  private final LogBackupProcessor logBackupProcessor;
+  private final LogBackupWriter logBackupWriter;
+  private final LogBackupProperties logBackupProperties;
+
+  @Bean(name = "logBackupJob")
+  public Job logBackupJob() {
+    return new JobBuilder("logBackupJob", jobRepository)
+        .listener(logBackupJobListener)
+        .start(logBackupStep())
+        .build();
+  }
+
+  @Bean
+  public Step logBackupStep() {
+    return new StepBuilder("logBackupStep", jobRepository)
+        .<LogContent, UploadPayload>chunk(logBackupProperties.chunkSize())
+        .transactionManager(transactionManager)
+        .reader(logBackupReader)
+        .processor(logBackupProcessor)
+        .writer(logBackupWriter)
+        .faultTolerant()
+        .retryLimit(logBackupProperties.retryLimit())
+        .retry(TransientDataAccessException.class)
+        .retry(LogBackupFailedException.class)
+        .skip(LogBackupFailedException.class)
+        .skipLimit(logBackupProperties.skipLimit())
+        .skipListener(skipLoggingListener)
+        .listener(logBackupStepListener)
+        .build();
+  }
+}
